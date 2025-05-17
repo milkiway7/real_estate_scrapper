1c7383c6df78837f1fbe551fac15eac44f69e2b2
alexm <alex.mleczkomt@gmail.com>
2025-05-17 13:25:13 +0200

    Scrapper OtoDom - constants

---
+++ ScrappersConfiguration/otodom_configuration.py
@@ -0,0 +1,12 @@
+OTODOM_CONFIGURATION = {
+    "url": "https://www.otodom.pl",
+    "cookies_button_selector": "Akceptuję",
+    "selectors": {
+        "find_city_button": '[data-cy="search.form.location.button"]',
+        "find_city_input": '[data-cy="search.form.location.input"]',
+        "search_button": '#search-form-submit',
+        "filter_dropdown_button": '[data-cy="]',
+        "filter_newest": '#react-select-listingSorting-option-1',
+        'offets_list': '[data-sentry-element="StyledList"]',
+    }
+}
